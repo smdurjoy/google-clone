0301610611c9a48c0e5cbbd5bca1578c43ee8083
smdurjoy <smdurjoy963@gmail.com>
2020-08-27 17:52:25 +0600

    Add Homepage UI design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './App.css'
+import Homepage from './pages/Homepage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world !</h1>
+    <div className="App"> 
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <Search/>
+          </Route>
+
+          <Route path="/">
+            <Homepage/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Homepage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
+import Search from '../components/Search';
+
+const Homepage = () => {
+	return(
+		<div className="home">
+			<div className="home__header">
+				<div className="home__headerLeft">
+					<Link to="/about">About</Link>
+					<Link to="/store">Store</Link>
+				</div>
+				<div className="home__headerRight">
+					<Link to="/gmail">Gmail</Link>
+					<Link to="/image">Image</Link>
+					<AppsIcon/>
+					<Avatar/>
+				</div>
+			</div>
+
+			<div className="home__body">
+				<img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"/>
+				<Search/>
+			</div>
+		</div>
+	)
+}
+
+export default Homepage;
